a48a806e48c94aabcb7e9b83e010cf06fed81cec
Aaron Whitlock <whitlock.aaron@yahoo.com>
2019-04-01 20:38:31 -0500

    random movie displays

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
+
+
+
+    
 
 @app.route("/")
 def index():
@@ -15,15 +20,29 @@ def index():
     content += "<li>" + movie + "</li>"
     content += "</ul>"
 
+    movie = get_random_movie()
+
     # TODO: pick another random movie, and display it under
     # the heading "<h1>Tommorrow's Movie</h1>"
+    day2 = "<h1>Tomorrow's Movie</h1>"
+    day2 +="<ul>"
+    day2 += "<li>" + movie + "</li>"
+    day2 += "</ul>"
 
-    return content
+    return content + day2
 
 def get_random_movie():
     # TODO: make a list with at least 5 movie titles
     # TODO: randomly choose one of the movies, and return it
-    return "The Big Lebowski"
-
+    movie = ["Star Wars Episode V:Empire Strikes Back", "Star Wars Episode IV: A New Hope","Star Wars Episode VI: Return of the Jedi"]
+    movie.append("A Star Wars Story: Rogue One")
+    movie.append("A Star Wars Story: Han solo")
+    movie.append("Star Wars Episode I: Phantom Menace")
+    movie.append("Star Wars Episode II: Clone Wars")
+    movie.append("Star Wars Episode III: Revenge of the Sith")
+    # more = input("Add a new movie to the list:")
+    # movie.append(more)
+    number = random.randrange(0, len(movie))
+    return movie[number]
 
 app.run()
